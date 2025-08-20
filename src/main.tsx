a9adb79cec34ed3f100c544794dd25fd48a774ca
Harshitha <harshithaprabhu06@gmail.com>
2025-08-21 00:03:35 +0530

    feat: migrate to React + Vite, updated README, and add some new Timeline features

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-// src/main.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // or App.css if you want
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
